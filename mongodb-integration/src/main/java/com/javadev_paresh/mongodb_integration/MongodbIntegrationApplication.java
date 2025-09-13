1bc0f47d127eeef1e9ccdb459fbd5836edab5888
javadevparesh <psavani@smartasset.com>
2025-09-13 18:18:33 -0400

    New springboot project created

---
+++ mongodb-integration/src/main/java/com/javadev_paresh/mongodb_integration/MongodbIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javadev_paresh.mongodb_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbIntegrationApplication.class, args);
+	}
+
+}
---
+++ mongodb-integration/src/test/java/com/javadev_paresh/mongodb_integration/MongodbIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev_paresh.mongodb_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
